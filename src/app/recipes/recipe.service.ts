1ed01efaff304588ad6b61717c52378cf3c9ea71
vijay narkademilli <vijaykumar.narkademilli@gmail.com>
2019-10-27 13:07:27 +0530

    Adding Recipes to Shopping List

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,18 @@
 import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
+import {EventEmitter,Injectable} from '@angular/core';
 import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 /**
  * Created by madhuri on 27-10-2019.
  */
-
+@Injectable()
 export class RecipeService{
 
-  recipeSelected = new EventEmitter<Recipe>();
+
+    constructor(private slService:ShoppingListService) {
+    }
+
+    recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe('Big Fat Burger',
@@ -30,6 +35,9 @@ export class RecipeService{
     return this.recipes.slice();
   }
 
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+      this.slService.addIngredients(ingredients);
+  }
 
 
 }
