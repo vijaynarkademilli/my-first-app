9f27aceca2635cfd68f9bcec731cd1de2f4d965d
vijay narkademilli <vijaykumar.narkademilli@gmail.com>
2019-10-27 10:26:14 +0530

    Shopping List Service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "./shopping-list.service";
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,14 +9,12 @@ import {Ingredient} from "../shared/ingredient.model";
 })
 export class ShoppingListComponent implements OnInit {
 
-  ingredients : Ingredient[] = [
-    new Ingredient('Apples',5),
-    new Ingredient('Tomatos',10),
-  ];
+  ingredients : Ingredient[];
 
-  constructor() { }
+  constructor(private shoppingListService:ShoppingListService) { }
 
   ngOnInit() {
+    this.ingredients = this.shoppingListService.getIngredients();
   }
 
   onIngredientAdded(ingredient:Ingredient){
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,18 @@
+import {Ingredient} from "../shared/ingredient.model";
 /**
  * Created by madhuri on 27-10-2019.
  */
+
+
+export class ShoppingListService{
+
+  private ingredients : Ingredient[] = [
+    new Ingredient('Apples',5),
+    new Ingredient('Tomatos',10),
+  ];
+
+  getIngredients(){
+    return this.ingredients.slice();
+  }
+
+}
